69c1829598a4a519284e003103e80473fdffc8ce
Carlos caudillo <charlyrod@gmail.com>
2025-02-03 19:31:09 -0600

    lazy loading in routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,50 +1,86 @@
-import {
-  getProducts,
-  getProductById,
-  createProduct,
-  updateProduct,
-  deleteProduct,
-} from "../controllers/products.controller.js";
-import {
-  productSchema,
-  productIdSchema,
-  productResponseSchema,
-} from "../schemas/products.shemas.js";
-
 const routes = [
   {
     method: "GET",
     url: "/products",
-    handler: getProducts,
-    schema: { response: productResponseSchema },
+    handler: async (req, reply) => {
+      const { getProducts } = await import(
+        "../controllers/products.controller.js"
+      );
+      return getProducts(req, reply);
+    },
+    schema: async () => {
+      const { productResponseSchema } = await import(
+        "../schemas/products.shemas.js"
+      );
+      return { response: productResponseSchema };
+    },
   },
   {
     method: "GET",
     url: "/products/:id",
-    handler: getProductById,
-    schema: { ...productIdSchema, response: productResponseSchema },
+    handler: async (req, reply) => {
+      const { getProductById } = await import(
+        "../controllers/products.controller.js"
+      );
+      return getProductById(req, reply);
+    },
+    schema: async () => {
+      const { productIdSchema, productResponseSchema } = await import(
+        "../schemas/products.shemas.js"
+      );
+      return { ...productIdSchema, response: productResponseSchema };
+    },
   },
   {
     method: "POST",
     url: "/products",
-    handler: createProduct,
-    schema: { ...productSchema, response: productResponseSchema },
+    handler: async (req, reply) => {
+      const { createProduct } = await import(
+        "../controllers/products.controller.js"
+      );
+      return createProduct(req, reply);
+    },
+    schema: async () => {
+      const { productSchema, productResponseSchema } = await import(
+        "../schemas/products.shemas.js"
+      );
+      return { ...productSchema, response: productResponseSchema };
+    },
   },
   {
     method: "PUT",
     url: "/products/:id",
-    handler: updateProduct,
-    schema: {
-      ...productIdSchema,
-      ...productSchema,
-      response: productResponseSchema,
+    handler: async (req, reply) => {
+      const { updateProduct } = await import(
+        "../controllers/products.controller.js"
+      );
+      return updateProduct(req, reply);
+    },
+    schema: async () => {
+      const { productIdSchema, productSchema, productResponseSchema } =
+        await import("../schemas/products.shemas.js");
+      return {
+        ...productIdSchema,
+        ...productSchema,
+        response: productResponseSchema,
+      };
     },
   },
   {
     method: "DELETE",
     url: "/products/:id",
-    handler: deleteProduct,
-    schema: { ...productIdSchema, response: productResponseSchema },
+    handler: async (req, reply) => {
+      const { deleteProduct } = await import(
+        "../controllers/products.controller.js"
+      );
+      return deleteProduct(req, reply);
+    },
+    schema: async () => {
+      const { productIdSchema, productResponseSchema } = await import(
+        "../schemas/products.shemas.js"
+      );
+      return { ...productIdSchema, response: productResponseSchema };
+    },
   },
 ];
 
